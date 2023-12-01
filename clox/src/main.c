5847ff189433d629e698a45215477e78d451ddb0
Josh Holbrook <josh.holbrook@gmail.com>
2023-11-30 19:20:55 -0900

    just me or did I return a constant? lmao

--- clox/src/main.c
+++ clox/src/main.c
@@ -1,14 +1,21 @@
 #include "common.h"
 #include "chunk.h"
 #include "debug.h"
+#include "vm.h"
 
 int main(int argc, const char* argv[]) {
+  initVM();
   Chunk chunk;
   initChunk(&chunk);
+  int constant = addConstant(&chunk, 1.2);
+  writeChunk(&chunk, OP_CONSTANT);
+  writeChunk(&chunk, constant);
   writeChunk(&chunk, OP_RETURN);
 
   disassembleChunk(&chunk, "test chunk");
+  interpret(&chunk);
 
+  freeVM();
   freeChunk(&chunk);
   return 0;
 }
