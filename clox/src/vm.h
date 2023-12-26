3991644789dfe615bf9be9050159fb8fb3540740
Josh Holbrook <josh.holbrook@gmail.com>
2023-12-26 13:07:45 -0900

    hash table, interned strings

--- clox/src/vm.h
+++ clox/src/vm.h
@@ -3,6 +3,7 @@
 
 #include "chunk.h"
 #include "value.h"
+#include "table.h"
 
 #define STACK_MAX 256
 
@@ -11,6 +12,8 @@ typedef struct {
   uint8_t* ip;
   Value stack[STACK_MAX];
   Value* stackTop;
+  // Remember how MSX BASIC has a "strings" area in memory?
+  Table strings;
   Obj* objects;
 } VM;
 
