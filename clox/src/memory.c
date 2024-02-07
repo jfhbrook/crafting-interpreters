b043ae8e27f7090d35b82ae7757b624e2e6efe5c
Josh Holbrook <josh.holbrook@gmail.com>
2024-02-06 16:48:32 -0900

    class object type

--- clox/src/memory.c
+++ clox/src/memory.c
@@ -85,6 +85,11 @@ static void blackenObject(Obj *object) {
   printf("\n");
 #endif
   switch (object->type) {
+  case OBJ_CLASS: {
+    ObjClass *cls = (ObjClass *)object;
+    markObject((Obj *)cls->name);
+    break;
+  }
   case OBJ_CLOSURE: {
     ObjClosure *closure = (ObjClosure *)object;
     // Mark the function we closed on
@@ -121,6 +126,10 @@ static void freeObject(Obj *object) {
   printf("%p free type %d\n", (void *)object, object->type);
 #endif
   switch (object->type) {
+  case OBJ_CLASS: {
+    FREE(ObjClass, object);
+    break;
+  }
   case OBJ_CLOSURE: {
     ObjClosure *closure = (ObjClosure *)object;
     // Doesn't own the upvalues themselves, just the array
